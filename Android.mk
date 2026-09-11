095c8ffeaa6022dca087770151bfad2c1acd887a
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-09-11 17:05:33 +0000

    klee: Initial vendor tree
    * Created per-device trees commit https://github.com/xiaomi-mt6899-dev/android_device_xiaomi_klee/tree/664b7fd9419947d4ca5c1a2b833968b2ad0b0e41

---
+++ Android.mk
@@ -0,0 +1,32 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),klee)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,350807e0a29e3977a93e331d484ef867fa8fe101)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,b252d3278336687d1a114d85368aa5ac7cb69552)
+$(call add-radio-file-sha1-checked,radio/ccu.img,7c67c65c613ea9601ec4931aaa2394ce3ffd21e5)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,2f73e2a851adfccafef4fd3a5586cf5ce4b52f84)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,ef08374f66823b9b7a9e333e28ebf2d93de43613)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,be5860b1371cdb572bb665e5eb7c34a2819180f1)
+$(call add-radio-file-sha1-checked,radio/dpm.img,647a11abdd7c1ed940976e2c9535f207b20376ae)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,0399a75f9fc5d081af4cafea82cffa98fca937db)
+$(call add-radio-file-sha1-checked,radio/gz.img,96f7d5d63c9f71e6d865ff5003701f355b7a8d2e)
+$(call add-radio-file-sha1-checked,radio/lk.img,1406ee555eec70c0f71b29b90b0ab91c70e085cf)
+$(call add-radio-file-sha1-checked,radio/logo.img,74e2f2c42a98e841fd34f38c89abf4849ff4fad0)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,eaaff18d3b25c4a58a6f1501cf5346572225bb13)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,ac5fe6a97fb71ab540c2f3cfc150c9c12574ab13)
+$(call add-radio-file-sha1-checked,radio/modem.img,98345244490bb8896a6f97cbb29a10418d964c68)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,59124122d221f6e9e060f220d0fc6ad4d081d3d0)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,50fbc6499b1b24f888f84fa285596b32f9fd9065)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,846e7e8fc26bc994d00b1c2211a22df4e6ce87b2)
+$(call add-radio-file-sha1-checked,radio/scp.img,ce9c81ae65a500a68b1e3ee87810c248d5cbbe0a)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,f0fe00132286262ecd9386c6d531ce308e9ba61e)
+$(call add-radio-file-sha1-checked,radio/sspm.img,47a80a44d59469ef3972a3d021d2bef3f42c1c81)
+$(call add-radio-file-sha1-checked,radio/tee.img,fbc344965c5eeef56a7d93c5513b80bc78a17ea6)
+$(call add-radio-file-sha1-checked,radio/vcp.img,42930499091ceae371837f11f58d943915d750d6)
+
+endif
